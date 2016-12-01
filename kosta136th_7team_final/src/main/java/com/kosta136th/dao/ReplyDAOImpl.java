60032c645c1326d4d8773d3fc52258430ac02492
forsnbs <forsnbs@gmail.com>
2016-12-01 17:53:50 +0900

    [add]  controller

--- kosta136th_7team_final/src/main/java/com/kosta136th/dao/ReplyDAOImpl.java
+++ kosta136th_7team_final/src/main/java/com/kosta136th/dao/ReplyDAOImpl.java
@@ -1,7 +1,6 @@
 package com.kosta136th.dao;
 
-import com.kosta136th.service.ReplyService;
 
-public class ReplyDAOImpl implements ReplyService {
+public class ReplyDAOImpl implements ReplyDAO {
 
 }
