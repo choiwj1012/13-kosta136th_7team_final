06a042985e4495e7a494281f9ea52fcc936f2c0b
lsj <storagepit@naver.com>
2016-12-07 14:34:41 +0900

    [bit002-1] error check

--- kosta136th_7team_final/src/main/java/com/kosta136th/user/UserController.java
+++ kosta136th_7team_final/src/main/java/com/kosta136th/user/UserController.java
@@ -1,5 +1,61 @@
 package com.kosta136th.user;
 
-public class UserController {
+import javax.inject.Inject;
+import javax.servlet.http.HttpSession;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class UserController {
+	@Inject
+	private UserService userService;
+	
+	@RequestMapping(value = "/requestSigninEmail", method = RequestMethod.POST)
+	@ResponseBody
+	public ResponseEntity<LoginSessionDTO> requestSigninEmail(@RequestBody LoginEmailDTO loginEmailDTO, HttpSession session){
+		
+		LoginSessionDTO loginSessionDTO = null;
+		ResponseEntity <LoginSessionDTO> entity = null;
+		System.out.println(loginEmailDTO.getEmail());
+		System.out.println(loginEmailDTO.getPassword());
+		System.out.println(loginEmailDTO.getNickname());
+		try { 
+			loginSessionDTO = userService.signinEmail(loginEmailDTO);
+			entity = new ResponseEntity<>(loginSessionDTO, HttpStatus.OK);
+			session.setAttribute("LoginSession", loginSessionDTO);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			entity = new ResponseEntity<>(loginSessionDTO, HttpStatus.BAD_REQUEST);
+			session.setAttribute("LoginSession", null);
+		}
+		
+		return entity;
+	}
+	
+	@RequestMapping(value = "/requestSignupEmail", method = RequestMethod.POST)
+	@ResponseBody
+	public  ResponseEntity<String> requestSignupEmail(@RequestBody LoginEmailDTO loginEmailDTO){
+		boolean signupSuccess = false;
+		ResponseEntity <String> entity = null;
+		try {
+			signupSuccess = userService.signupEmail(loginEmailDTO);
+			if (signupSuccess == true){
+				entity = new ResponseEntity<>("성공했습니다", HttpStatus.OK);
+			} else{
+				entity = new ResponseEntity<>("실패했습니다", HttpStatus.BAD_REQUEST);
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			entity = new ResponseEntity<>("실패했습니다", HttpStatus.BAD_REQUEST);
+		}
+		return entity;
+	}
 }
--- kosta136th_7team_final/src/main/java/com/kosta136th/user/UserDAOImpl.java
+++ kosta136th_7team_final/src/main/java/com/kosta136th/user/UserDAOImpl.java
@@ -1,5 +1,39 @@
 package com.kosta136th.user;
 
-public class UserDAOImpl implements UserDAO{
+import javax.inject.Inject;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserDAOImpl implements UserDAO{
+	@Inject
+	private SqlSession session;
+	private static String namespace = "com.kosta136th.mappers.UserMapper";
+	
+	@Override
+	public LoginSessionDTO signinEmail(LoginEmailDTO loginEmailDTO) throws Exception {
+		// TODO Auto-generated method stub
+		LoginSessionDTO loginSessionDTO = null;
+		loginSessionDTO = session.selectOne(namespace + ".getLoginProfileByEmail", loginEmailDTO);
+		return loginSessionDTO;
+	}
+	
+	@Override
+	public boolean signupEmail(LoginEmailDTO loginEmailDTO) {
+		// TODO Auto-generated method stub
+		int affectedRows = 0;
+		boolean signupSuccess = false;
+		System.out.println(loginEmailDTO.getEmail());
+		
+		affectedRows = session.insert(namespace + ".insertLoginProfileByEmail", loginEmailDTO);
+		
+		if (affectedRows > 0){
+			signupSuccess = true;
+		}else{
+			signupSuccess = false;
+		}
+		
+		return signupSuccess;
+	}
 }
