ab0170ed0aac66234a98013ba247a09f23f6e3f8
hyeonwoo1129 <hyeonwoo1129@naver.com>
2016-12-07 14:30:28 +0900

    [bit003-01 controller]

--- kosta136th_7team_final/src/main/java/com/kosta136th/marketPrice/MarketPriceController.java
+++ kosta136th_7team_final/src/main/java/com/kosta136th/marketPrice/MarketPriceController.java
@@ -1,6 +1,33 @@
 package com.kosta136th.marketPrice;
 
+import javax.inject.Inject;
 
+import org.apache.log4j.spi.LoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/marketPrice")
 public class MarketPriceController {
 
+	private static final Logger logger = LoggerFactory.getLogger(MarketPriceController.class);
+	
+	@Inject
+	private MarketPriceService marketPriceService;
+	
+	@RequestMapping(value = "/rate", method = RequestMethod.GET)
+	public String callRateoutput(Model model) {
+		
+		marketPriceService.rate(marketPriceList, rateList);
+		
+		model.addAttribute("marketPriceList", marketPriceList);
+		model.addAttribute("rateList", rateList);
+		
+		return "/sub/graph";
+		
+	}
+	
 }
