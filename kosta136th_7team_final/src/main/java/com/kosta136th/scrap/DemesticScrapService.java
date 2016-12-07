6f35908a31238a7ea8a18e33d524a044b144e913
pcj9027 <pcj9027@naver.com>
2016-12-07 16:23:40 +0900

    [bit004-01] naver api

--- kosta136th_7team_final/src/main/java/com/kosta136th/scrap/DemesticScrapService.java
+++ kosta136th_7team_final/src/main/java/com/kosta136th/scrap/DemesticScrapService.java
@@ -1,5 +1,9 @@
 package com.kosta136th.scrap;
 
+import java.util.List;
+
 public interface DemesticScrapService {
+	
+	public List<DomesticScrap> searchNews(String keyword, int display, int start) throws Exception;
 
 }
