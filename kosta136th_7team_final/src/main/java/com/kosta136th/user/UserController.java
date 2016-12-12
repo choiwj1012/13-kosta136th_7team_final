a3d6d0aa3aedb2bfc0378f49fef6a9f9e377df19
lsj <storagepit@naver.com>
2016-12-12 10:20:54 +0900

    [bit002-01]

--- kosta136th_7team_final/src/main/java/com/kosta136th/user/UserController.java
+++ kosta136th_7team_final/src/main/java/com/kosta136th/user/UserController.java
@@ -8,16 +8,28 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.security.SecureRandom;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
+import java.util.Random;
 
 import javax.inject.Inject;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -56,7 +68,11 @@ public class UserController {
 			signinSessionDTO = userService.signinEmail(signinEmailDTO);
 			entity = new ResponseEntity<>(signinSessionDTO, HttpStatus.OK);
 			//로그
-			System.out.println(signinSessionDTO.toString());
+			if (signinSessionDTO != null){
+				System.out.println(signinSessionDTO.toString());
+			}else{
+				System.out.println("User[null]");
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 			entity = new ResponseEntity<>(signinSessionDTO, HttpStatus.BAD_REQUEST);
@@ -69,9 +85,10 @@ public class UserController {
 	
 	@RequestMapping(value = "/requestSignupEmail", method = RequestMethod.POST, produces = "application/text; charset=utf8")
 	@ResponseBody
-	public ResponseEntity<String> requestSignupEmail(@RequestBody User signinEmailDTO) throws Exception{
+	public ResponseEntity<String> requestSignupEmail(@RequestBody User signupEmailDTO, String authentication, HttpSession session) throws Exception{
 		
 		boolean signupSuccess = false;
+		
 		
 		//한글 인코딩을 위해, Restful 방식에서 어쩔 수 없이 쓴다.
 		//이 반복을 줄일 방법이 없을까?
@@ -79,14 +96,69 @@ public class UserController {
 		responseHeaders.add("Content-Type", "text/html; charset=UTF-8");
 		ResponseEntity <String> entity = null;
 		
-		signupSuccess = userService.signupEmail(signinEmailDTO);
+		try{
 			
-		if (signupSuccess == true){
-			entity = new ResponseEntity<>("성공했습니다", HttpStatus.OK);
-		} else{
-			entity = new ResponseEntity<>("실패했습니다", HttpStatus.BAD_REQUEST);
+		System.out.println(signupEmailDTO.toString());
+		System.out.println("인증 : " + authentication);
+		System.out.println("세션 저장 인증 : " + (String)session.getAttribute("authentication"));
+		//인증번호가 틀리면 그냥 돌아가
+		if (authentication != session.getAttribute("authentication")){
+			entity = new ResponseEntity<String>("잘못된 인증번호입니다", responseHeaders, HttpStatus.BAD_REQUEST);
+			return entity;
 		}
 		
+		String email = signupEmailDTO.getEmail();
+		//DAO에 접근. 검사 로직.
+		//네이버 아이디와 일치하는 행을 불러오는 것
+		String email_state;
+		email_state = checkEmailDuplication(email);
+		
+		//email_state = 0 : DB에 있는 이메일
+		//email_state = 1 : DB에 없는 이메일
+		//email_state = 2 : 기타(오류)
+		
+		switch (email_state){
+			
+			case "0":
+				entity = new ResponseEntity<String>("이미 가입한 이메일입니다", responseHeaders, HttpStatus.BAD_REQUEST);
+				break;
+			case "2":
+				entity = new ResponseEntity<String>("에러가 발생했습니다", responseHeaders, HttpStatus.BAD_REQUEST);
+				break;
+			case "1":
+				//DAO에 접근. 비즈니스 로직
+				//네이버 회원 가입
+				//로그인에 실패했습니다는 불가능
+				//오류가 발생했습니다는 가능
+				String message = "회원 가입에 실패했습니다";
+				try{
+					User signupEmailVO = new User(signupEmailDTO.getEmail(),"",signupEmailDTO.getNickname());
+					System.out.println(signupEmailVO.toString());
+					
+					signupSuccess = userService.signupNaver(signupEmailVO);
+										
+					if (signupSuccess){
+						entity = new ResponseEntity<>("회원가입에 성공했습니다", HttpStatus.OK);
+						setSigninSessionAttribute(session, signupEmailVO);
+					} else{
+						entity = new ResponseEntity<>("실패했습니다", HttpStatus.BAD_REQUEST);
+					}
+					
+				} catch(Exception e) {
+					message = "오류가 발생했습니다.";							
+				}
+				//리턴 문자열 : "성공" 또는 "실패"
+				entity = new ResponseEntity<String>(message, responseHeaders, HttpStatus.BAD_REQUEST);
+				break;
+			default :
+				entity = new ResponseEntity<String>("에러가 발생했습니다", responseHeaders, HttpStatus.BAD_REQUEST);
+				break;
+				
+		}
+		}catch(Exception e){
+			System.out.println("아래의 오류가 났다.");
+			e.printStackTrace();
+		}
 		return entity;		
 	}
 	
@@ -112,6 +184,13 @@ public class UserController {
 		apiURL = generateNaverLoginAPI("/doSigninNaver");
 		
 		return apiURL;		
+	}
+	
+	//'로그아웃' 버튼 클릭
+	@RequestMapping(value = "/requestSignout", method = RequestMethod.POST, produces = "application/text; charset=utf8")
+	@ResponseBody
+	public void requestSignout(HttpSession httpSession){
+		setSigninSessionAttribute(httpSession, null);	
 	}
 	
 	//콜백 주소(목적지 : destination)를 입력받아
@@ -324,6 +403,11 @@ public class UserController {
 		}
 		
 		signinSessionDTO = signupNaverVO;
+		if (signinSessionDTO != null){
+		System.out.println("세션 저장 직전 : " + signinSessionDTO.toString());
+		}else{
+			System.out.println("세션 저장 직전 : " + "null");			
+		}
 		setSigninSessionAttribute(httpSession, signinSessionDTO);
 		
 		return entity;
@@ -503,10 +587,183 @@ public class UserController {
 			}
 			
 			setSigninSessionAttribute(httpSession, signinSessionDTO);
+			System.out.println("세션 저장 직전 : " + signinSessionDTO.toString());
 			return entity;
 			
 		}
 	
+	//인증번호 발송
+	@RequestMapping(value = "/requestAuthentication", method = RequestMethod.POST)
+	@ResponseBody
+	public String requestAuthentication(@ModelAttribute User SigninEmailDTO, HttpSession httpSession){
+		try {
+			String registerPassword = issueTemporaryPassword();					
+			String recipient = SigninEmailDTO.getEmail();
+
+			//세션에 수정된 password를 입력한다
+			httpSession.setAttribute("authentication", registerPassword);
+			
+			//메일을 보낸다
+			String subject = "인증번호가 발송되었습니다";
+			String body = "인증번호는 " + registerPassword + "입니다";
+			sendEmail(recipient, subject, body);
+			
+			return "메일이 발송되었습니다";
+		} catch (AddressException e) {
+			e.printStackTrace();
+			return "AddressException 오류가 발생했습니다";				
+		} catch (MessagingException e) {
+			e.printStackTrace();
+			return "MessagingException 오류가 발생했습니다";				
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "오류가 발생했습니다.";
+		}
+	}
+	
+	//임시 비밀번호 발송
+		@RequestMapping(value = "/requestIssuePassword", method = RequestMethod.POST)
+		@ResponseBody
+		public String requestIssuePassword(@ModelAttribute User SigninEmailDTO){
+			try {
+				String registerPassword = issueTemporaryPassword();					
+				String recipient = SigninEmailDTO.getEmail();
+
+				//db에 수정된 password를 입력한다
+				SigninEmailDTO.setPassword(registerPassword);
+				
+				userService.updateUserPassword(SigninEmailDTO);
+
+				//메일을 보낸다
+				String subject = "인증번호가 발송되었습니다";
+				String body = "인증번호는 " + registerPassword + "입니다";
+				sendEmail(recipient, subject, body);
+				
+				return registerPassword;
+			} catch (AddressException e) {
+				e.printStackTrace();
+				return "AddressException 오류가 발생했습니다";				
+			} catch (MessagingException e) {
+				e.printStackTrace();
+				return "MessagingException 오류가 발생했습니다";				
+			} catch (Exception e) {
+				e.printStackTrace();
+				return "오류가 발생했습니다.";
+			}
+		}
+	
+	//새로운 비밀번호를 생성하는 메서드
+	//특수문자, 영문, 숫자를 꼭 1개씩 모두 갖고 있어야 한다.
+	private String issueTemporaryPassword() {
+		Random random = new Random();
+		int password_length = 8 + random.nextInt(9);
+
+		ArrayList<String> chars = new ArrayList<String>();
+		
+		ArrayList<String> password = new ArrayList<String>();
+
+		String alphabets[] = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z".split(",");
+
+		String specialChars[] = "!,@,#,$,%,^,&,*,?,(,),{,},[,],:,;,<,>,.,/,|".split(",");
+
+		String nums[] = "0,1,2,3,4,5,6,7,8,9".split(",");
+		 
+		String p = alphabets[random.nextInt(alphabets.length)];
+
+		String q = specialChars[random.nextInt(specialChars.length)];
+
+		String r = nums[random.nextInt(nums.length)];
+		
+		for (int i = 0; i < alphabets.length; i++){
+			chars.add(alphabets[i]);
+		}
+		
+		for (int i = 0; i < specialChars.length; i++){
+			chars.add(specialChars[i]);
+		}
+		
+		for (int i = 0; i < nums.length; i++){
+			chars.add(nums[i]);
+		}
+		
+		StringBuffer buffer = new StringBuffer();
+
+		password.add(p);
+		password.add(q);
+		password.add(r);
+		
+		for (int i = 3; i < password_length; i++) {
+			password.add(chars.get(random.nextInt(chars.size())));
+		}
+		
+		for (int i = 0; i < password.size(); i++){
+			System.out.print(password.get(i));
+		}
+		System.out.println();
+		
+		for (int i = password_length - 1; i > 0; i--){
+
+			int num = random.nextInt(i);
+
+			String a = password.get(i);	
+			String b = password.get(num); 
+			String temp;
+			
+			temp = a;
+			a = b;
+			b = temp;
+			
+			password.set(i, a);
+			password.set(num, b);
+		}
+		
+		for (int i = 0; i < password.size(); i++){
+			buffer.append(password.get(i));
+		}
+		
+		System.out.println(buffer.toString());
+		
+		return buffer.toString();
+	}
+
+	//메일을 보낸다
+	//이메일을 발송하는 메서드
+	// recipient : 우리 회사의 이메일.
+	// subject : 보낼 이메일의 제목
+	// body : 보낼 이메일의 본문.
+	public void sendEmail(String recipient, String subject, String body) throws AddressException, MessagingException {
+		
+		String host = "smtp.naver.com";
+		final String username = "storagepit";       //@naver.com을 뺀 발신자(회사)의 이메일 번호
+		final String password = "R31676003";   //비밀번호를 여기에 입력하세요
+		int port=465; //네이버에서 확인할 것
+		 
+		Properties props = System.getProperties(); 
+		 
+		props.put("mail.smtp.host", host);
+		props.put("mail.smtp.port", port);
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.ssl.enable", "true");
+		props.put("mail.smtp.ssl.trust", host);
+		   
+		Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
+			String un = username;
+			String pw = password;
+			protected javax.mail.PasswordAuthentication getPasswordAuthentication() {
+				return new javax.mail.PasswordAuthentication(un, pw);
+			}
+		});
+		session.setDebug(true); //for debug
+		   
+		Message mimeMessage = new MimeMessage(session); 
+		mimeMessage.setFrom(new InternetAddress("storagepit@naver.com")); //발송자(회사)의 풀 이메일로 입력
+		mimeMessage.setRecipient(Message.RecipientType.TO, new InternetAddress(recipient)); 
+		mimeMessage.setSubject(subject);  
+		mimeMessage.setText(body);        
+		Transport.send(mimeMessage);
+		
+	}
+		
 	//이메일 중복 검사 컨트롤러
 	public String checkEmailDuplication(String email){
 		//email_state = 0 : DB에 있는 이메일
@@ -566,6 +823,26 @@ public class UserController {
 	//세션 속성을 바꾼다
 	public void setSigninSessionAttribute(HttpSession session, User signinSessionDTO){
 		session.setAttribute("signinSessionDTO", signinSessionDTO);
+	}
+
+	//세션 속성을 반환한다
+	@RequestMapping(value = "/requestSigninSessionAttribute", method = RequestMethod.POST)
+	@ResponseBody
+	public ResponseEntity<User> requestSigninSessionAttribute(HttpSession httpSession){
+		System.out.println("세션 정보 요청이 옴");
+		ResponseEntity<User> entity = null;
+		HttpHeaders responseHeaders = new HttpHeaders();
+	    responseHeaders.add("Content-Type", "text/html; charset=UTF-8");
+
+	    User signinSessionDTO = (User)httpSession.getAttribute("signinSessionDTO");
+	    
+	    if (signinSessionDTO != null){
+	    	System.out.println(signinSessionDTO.toString());
+		}else{
+			System.out.println("User [null]");
+		}
+		entity = new ResponseEntity<>(signinSessionDTO, HttpStatus.OK);
+		return entity;
 	}
 
 	//도메인을 새로 만들지 않기 위한 내부 클래스. 
