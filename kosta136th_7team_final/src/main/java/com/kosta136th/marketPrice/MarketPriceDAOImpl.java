e4244f25b60a77ede0bbb4e222af63ae6528804d
hyeonwoo1129 <hyeonwoo1129@naver.com>
2016-12-13 09:50:13 +0900

    [bit003-02]

--- kosta136th_7team_final/src/main/java/com/kosta136th/marketPrice/MarketPriceDAOImpl.java
+++ kosta136th_7team_final/src/main/java/com/kosta136th/marketPrice/MarketPriceDAOImpl.java
@@ -10,14 +10,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class MarketPriceDAOImpl implements MarketPriceDAO{
 
-<<<<<<< HEAD
-	
-	
-	@Override
-	public void rateSave(MarketPrice marketPrice) throws Exception {
-		
-		
-=======
 	@Inject
 	private SqlSession sqlSession;
 	
@@ -42,14 +34,14 @@ public class MarketPriceDAOImpl implements MarketPriceDAO{
 	public List<MarketPrice> coinRateList() throws Exception {
 		
 		return sqlSession.selectList(namespace + ".bitcoinList");
->>>>>>> 68931bff15b818f1830116c0a776d19068dfc30a
+
 		
 	}
 
 	@Override
 	public List<MarketPriceChart> oneChart(String money_type) throws Exception {
 		
-		return sqlSession.selectList(namespace + ".oneChart");
+		return sqlSession.selectList(namespace + ".oneChart", money_type);
 		
 	}
 
--- kosta136th_7team_final/src/main/java/com/kosta136th/marketPrice/MarketPriceServiceImpl.java
+++ kosta136th_7team_final/src/main/java/com/kosta136th/marketPrice/MarketPriceServiceImpl.java
@@ -1,36 +1,25 @@
 package com.kosta136th.marketPrice;
 
-<<<<<<< HEAD
 import javax.inject.Inject;
 
-=======
 import java.util.List;
-
-import javax.inject.Inject;
 
 import org.springframework.stereotype.Service;
 
 @Service
->>>>>>> 68931bff15b818f1830116c0a776d19068dfc30a
+
 public class MarketPriceServiceImpl implements MarketPriceService{
 
 	@Inject
 	MarketPriceDAO dao;
 	
 	@Override
-<<<<<<< HEAD
-	public void rateSave(MarketPrice marketPrice) throws Exception {
-		
-=======
 	public void rateSave(MarketPriceSave marketPrice) throws Exception {
 
->>>>>>> 68931bff15b818f1830116c0a776d19068dfc30a
 		dao.rateSave(marketPrice);
 		
 	}
 
-<<<<<<< HEAD
-=======
 	@Override
 	public List<MarketPriceChart> chart(String money_type) throws Exception {
 		
@@ -45,7 +34,6 @@ public class MarketPriceServiceImpl implements MarketPriceService{
 
 	}
 
-<<<<<<< HEAD
 	@Override
 	public List<MarketPriceChart> oneChart(String money_type) throws Exception {
 		
@@ -53,7 +41,4 @@ public class MarketPriceServiceImpl implements MarketPriceService{
 		
 	}
 
-=======
->>>>>>> 68931bff15b818f1830116c0a776d19068dfc30a
->>>>>>> 1ca1d6b44d4835eff6b2fce4b90164208e285677
 }
