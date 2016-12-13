9368ffaba295e113186da57211708e02470361d4
lsj <storagepit@naver.com>
2016-12-13 15:58:24 +0900

    [bit002-01]modal, dealer+member

--- kosta136th_7team_final/src/main/java/com/kosta136th/user/UserService.java
+++ kosta136th_7team_final/src/main/java/com/kosta136th/user/UserService.java
@@ -2,7 +2,7 @@ package com.kosta136th.user;
 
 public interface UserService {
 	public User signinEmail(User loginEmailDTO) throws Exception;
-	public boolean signupEmail(User loginEmailDTO) throws Exception;
+	public boolean signupEmail(User loginEmailDTO, String register_type_code) throws Exception;
 	public String checkEmailDuplication(String email) throws Exception;
 	public String checkNicknameDuplication(String nickname) throws Exception;
 	public User signinNaver(String email) throws Exception;
