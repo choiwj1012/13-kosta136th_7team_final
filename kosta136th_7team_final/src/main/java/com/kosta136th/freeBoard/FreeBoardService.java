07fd36b94f6cd9b75737fde69bc7cbac2a8704c9
forsnbs <forsnbs@gmail.com>
2016-12-13 18:11:11 +0900

    [freeBoard]make package and mapper

---
+++ kosta136th_7team_final/src/main/java/com/kosta136th/freeBoard/FreeBoardService.java
@@ -0,0 +1,14 @@
+package com.kosta136th.freeBoard;
+
+import java.util.List;
+
+public interface FreeBoardService {
+	
+public void regist(FreeBoard freeBoard) throws Exception;
+	
+	public FreeBoard read(Integer freeBoard_Num) throws Exception;
+	
+//	public void modify(FreeBoard freeBoard) throws Exception;
+	
+	public List<FreeBoard> listAll() throws Exception;
+}
