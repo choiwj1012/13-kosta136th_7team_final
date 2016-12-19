acbc77099be20d9d08b528a2fb1a8a711ca7efc8
WonJae Choi <choiwj1012@hanmail.net>
2016-12-19 09:39:31 +0900

    오류처리중

--- kosta136th_7team_final/src/main/webapp/resources/js/marketPrice_chart.js
+++ kosta136th_7team_final/src/main/webapp/resources/js/marketPrice_chart.js
@@ -6,14 +6,11 @@ $(document).ready(function () {
 	// 처음 시작시 PRICE_BTC값이 나타나는 쿼리
 	var url = "/rate/bitrate?money_type=" + money_type + "&sorting_type=" + sorting_type;
 	
-	
-	
-	
 	$.getJSON(url, function (data) {
 	
 		var str = "";
 			$.each(data, function(){
-//					.reverse()
+
         	    str += "<tr class='table_row' id='table_price_row'>";
                 str += "<td>" + this.label + "</td>";
                 str += "<td>" + this.name + "</td>";
@@ -87,7 +84,6 @@ $(document).ready(function () {
 			money_name_option = "BTC";
 		} 
 		
-		
 		$.ajax({
 			
 			url: "/rate/bitrate/",				//목적지 URI	//Controller로 보낸다.
@@ -129,128 +125,6 @@ $(document).ready(function () {
 		
 		var options = {
 
-				rangeSelector: {
-					
-					//기간 버튼 설정
-					buttons: [
-						{
-		                    type: 'hour',
-		                    count: 12,
-		                    text: '12h'
-		                }, {
-		                    type: 'day',
-		                    count: 1,
-		                    text: '1D'
-		                }, {
-		                    type: 'day',
-		                    count: 3,
-		                    text: '3D'
-		                }, {
-		                    type: 'day',
-		                    count: 7,
-		                    text: '1w'
-		                }, {
-		                    type: 'all',
-		                    count: 1,
-		                    text: 'All'
-		                }],
-					//기간 버튼 Default 값 설정 //0: 가장 첫 기간버튼, 1: 두 번째 기간버튼, 2: 세...
-					selected: 4,
-		          },
-				
-				chart: {
-					// 뿌려줄 View단의 id 설정
-					renderTo : 'chart',
-					// 그래프 타입(디자인) 설정
-					// type : 'area'
-				},
-				
-				series : [{
-					name: money_name_option,
-		            tooltip: {
-		            valueDecimals: 3,
-		            type: 'area'
-		            }
-				}]			
-			};
-		
-		$.ajax({
-			
-			url : "/rate/oneChart/",
-			type : 'get',
-			data : {"coin_type" : coin_type, "money_type" : money_type},
-			
-			success : function() {
-				
-				var url = "/rate/oneChart?coin_type=" + coin_type + "&money_type=" + money_type;
-
-				$.getJSON(url, function(data) {
-					
-					options.series[0].data = data;
-					var chart = new Highcharts.stockChart(options);
-					
-				});
-				
-			}
-			
-		});
-						
-	});
-	
-	
-	/* 정렬 타입 선택 콤보박스 메소드 */
-	// 디폴트 상태 : 24시간 거래량 순
-	$("#combo-box_sortingType").on('change', function(){
-		
-		money_type = $("#combo-box_moneyType").find(":selected").val();
-		sorting_type = $(this).find(":selected").val();
-		
-		$.ajax({
-			
-			url: "/rate/bitrate/",				//목적지 URI	//Controller로 보낸다.
-			//async : false,						//동기방식
-			type: 'get',							//get 타입 (post타입 등이 있음)
-			data: {"money_type" : money_type, "sorting_type" : sorting_type},		//money_type을 넘긴다.
-			
-			success:  function () {				//성공시 return된 객체를
-				
-				var url = "/rate/bitrate?money_type=" + money_type + "&sorting_type=" + sorting_type;		//MarketPriceDataController로 부터 받은 데이터를 처리한다.
-
-				
-				$.getJSON(url,  function (data) {
-					
-					var str = "";
-				                                                                                                      
-	                $.each(data, function(){
-	               	    
-	               	    str += "<tr class='table_row' id='table_price_row'>";
-	                    str += "<td>" + this.label + "</td>";
-	                    str += "<td>" + this.name + "</td>";
-		                str += "<td>" + this.price + "</td>";
-		                str += "<td>" + this.volume_24h + "</td>";
-	                    str += "</tr>"
-	                    
-	                });       
-
-	                $("#bitrate").html(str);
-
-			  });
-				
-			}
-			
-		});
-						
-	});	
-	
-	
-	
-	
-	/******* CHART 시작 ********/
-	// options.series[0].data  = data; 코드 때문에 수시로 초기화해줘야한다.
-	// document - ready 상태일 때
-	// default 상세일 때
-	var options = {
-
 			rangeSelector: {
 				
 				//기간 버튼 설정
@@ -288,23 +162,140 @@ $(document).ready(function () {
 			},
 			
 			series : [{
-				name: 'USD',
+				name: money_name_option,
 	            tooltip: {
-	            valueDecimals: 3
+	            valueDecimals: 3,
+	            type: 'area'
 	            }
 			}]			
 		};
+		
+		$.ajax({
+			
+			url : "/rate/oneChart/",
+			type : 'get',
+			data : {"coin_type" : coin_type, "money_type" : money_type},
+			
+			success : function() {
+				
+				var url = "/rate/oneChart?coin_type=" + coin_type + "&money_type=" + money_type;
+
+				$.getJSON(url, function(data) {
+					
+					options.series[0].data = data;
+					var chart = new Highcharts.stockChart(options);
+					
+				});
+				
+			}
+			
+		});
+						
+	});
+	
+	
+	/* 정렬 타입 선택 콤보박스 메소드 */
+	// 디폴트 상태 : 24시간 거래량 순
+	$("#combo-box_sortingType").on('change', function(){
+		
+		money_type = $("#combo-box_moneyType").find(":selected").val();
+		sorting_type = $(this).find(":selected").val();
+		
+		$.ajax({
+			
+			url: "/rate/bitrate/",				//목적지 URI	//Controller로 보낸다.
+			type: 'get',							//get 타입 (post타입 등이 있음)
+			data: {"money_type" : money_type, "sorting_type" : sorting_type},		//money_type을 넘긴다.		
+			success:  function () {				//성공시 return된 객체를
+				
+				var url = "/rate/bitrate?money_type=" + money_type + "&sorting_type=" + sorting_type;		//MarketPriceDataController로 부터 받은 데이터를 처리한다.
+			
+				$.getJSON(url,  function (data) {
+					
+					var str = "";
+				                                                                                                      
+	                $.each(data, function(){
+	               	    
+	               	    str += "<tr class='table_row' id='table_price_row'>";
+	                    str += "<td>" + this.label + "</td>";
+	                    str += "<td>" + this.name + "</td>";
+		                str += "<td>" + this.price + "</td>";
+		                str += "<td>" + this.volume_24h + "</td>";
+	                    str += "</tr>"
+	                    
+	                });       
+
+	                $("#bitrate").html(str);
+
+			  });
+				
+			}
+			
+		});
+						
+	});	
+	
+	
+	/******* CHART 시작 ********/
+	// default 상세일 때
+	var options = {
+
+		rangeSelector: {
+			
+			//기간 버튼 설정
+			buttons: [
+				{
+                    type: 'hour',
+                    count: 12,
+                    text: '12h'
+                }, {
+                    type: 'day',
+                    count: 1,
+                    text: '1D'
+                }, {
+                    type: 'day',
+                    count: 3,
+                    text: '3D'
+                }, {
+                    type: 'day',
+                    count: 7,
+                    text: '1w'
+                }, {
+                    type: 'all',
+                    count: 1,
+                    text: 'All'
+                }],
+		
+			selected: 4,
+			
+          },
+		
+		chart: {
+			// 뿌려줄 View단의 id 설정
+			renderTo : 'chart',
+			// 그래프 타입(디자인) 설정
+			// type : 'area'
+		},
+		
+		series : [{
+			name: 'USD',
+            tooltip: {
+            valueDecimals: 3
+            }
+		}]
+		
+	};
 	
 	//MarketPriceDataController.java
-		//@RequestMapping(value = "/chartData") ++ @RequestMapping("/rate/*")
-		//초기값은 btc로 설정.
-		var chartUrl = "/rate/oneChart?coin_type=Bitcoin&money_type=" + money_type;		//MarketPriceDataController로 부터 받은 데이터를 처리한다.
-		$.getJSON(chartUrl,  function (data) {
-			
-			options.series[0].data  = data;
-			var chart = new Highcharts.stockChart(options);
-		  
-		});
+	//@RequestMapping(value = "/chartData") ++ @RequestMapping("/rate/*")
+	//초기값은 btc로 설정.
+	var chartUrl = "/rate/oneChart?coin_type=Bitcoin&money_type=" + money_type;		//MarketPriceDataController로 부터 받은 데이터를 처리한다.
+	$.getJSON(chartUrl,  function (data) {
+		
+		options.series[0].data  = data;
+		var chart = new Highcharts.stockChart(options);
+	  
+	});
 		
 	
 	// 코인리스트 row를 클릭했을 때
