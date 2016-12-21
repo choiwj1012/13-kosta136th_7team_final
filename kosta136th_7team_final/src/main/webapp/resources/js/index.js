4eeacb8b8c2d9b90721b9eb99cc4e3b54c7ad234
wonjae choi <choiwj1012@hanmail.net>
2016-12-21 21:21:58 +0900

    Merge remote-tracking branch 'refs/remotes/origin/master' into pr/68
    # Conflicts:
    #	kosta136th_7team_final/src/main/webapp/resources/js/index.js

--- kosta136th_7team_final/src/main/webapp/resources/js/index.js
+++ kosta136th_7team_final/src/main/webapp/resources/js/index.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function() {
 	var options = {
 		videoId: 'H9P_wkq08XA', 
@@ -8,34 +7,4 @@ $(document).ready(function() {
 
 	$('#wrapper').tubular(options);
 	
-<<<<<<< HEAD
-});
-=======
 }); 
-
-function getEmail() {
-	
-	var email = $("#email").val();
-	var pattern = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-	if( pattern.test(email) ) 
-	{
-		alert("메일 주소가 올바르게 입력되었습니다." + email);
-		$.ajax({
-			type : 'post',
-			url : '/getEmail',
-			headers :{
-				"Content-Type" : "application/json",
-				"X-HTTP-Method_Overrride" : "POST",
-			},
-			dataType : 'text',
-			data : JSON.stringify(email),
-		});
-	} 
-	else 
-	{
-		alert("메일 주소가 유효하지 않습니다." + email);
-	}
-	
-}
-
->>>>>>> refs/remotes/origin/master
