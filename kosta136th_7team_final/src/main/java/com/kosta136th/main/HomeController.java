4e84c5e6eab7fbe3f3fe540ac0db2174f2978ee0
WonJae Choi <choiwj1012@hanmail.net>
2016-12-26 09:26:27 +0900

    프로젝트 폴더 정리중

--- kosta136th_7team_final/src/main/java/com/kosta136th/main/HomeController.java
+++ kosta136th_7team_final/src/main/java/com/kosta136th/main/HomeController.java
@@ -22,40 +22,4 @@ public class HomeController {
 		
 	}
 	
-	
-	// 자유게시판 리스트 경로
-	@RequestMapping(value = "/board_list", method = RequestMethod.GET)
-	public String boardList(){
-		
-		return "sub/freeboard/board_list";
-		
-	}
-	
-	
-	// 자유게시판 읽기 경로
-	@RequestMapping(value = "/board_read", method = RequestMethod.GET)
-	public String boardRead(){
-		
-		return "sub/freeboard/board_read";
-		
-	}
-	
-	
-	// 자유게시판 쓰기 경로
-	@RequestMapping(value = "/board_write", method = RequestMethod.GET)
-	public String boardWrite(){
-		
-		return "sub/freeboard/board_write";
-		
-	}
-	
-	
-	// 자유게시판 수정 경로
-	@RequestMapping(value = "/board_update", method = RequestMethod.GET)
-	public String boardUpdate(){
-		
-		return "sub/freeboard/board_update";
-		
-	}
-	
 }
